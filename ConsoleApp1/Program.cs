92fc76700b20799509f26a8f7a8fa75f2f2d3a59
Олександр <@DESKTOP-S660HFA>
2022-02-18 13:55:37 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Address someAddress = new Address();
+
+            someAddress.Index = 18766;
+            someAddress.Country = "Ukraine";
+            someAddress.City = "Kyiv";
+            someAddress.Street = "Kolotyshkina";
+            someAddress.House = 3;
+            someAddress.Apartment = 66;
+
+            Console.WriteLine(someAddress.GetAddress());
+
+
+            Console.ReadKey();
+        }
+    }
+}
